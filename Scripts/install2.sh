49391499ca1cceaccb98f50b48844151a8f0bfbb
unknown <ol.melancon@gmail.com>
2018-03-31 14:24:35 -0400

    init repo for vs

---
+++ Scripts/install2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ${0%/*}
+RW_PATH="/c/Program Files (x86)/Steam/steamapps/common/RimWorld"
+cp --verbose -rf ../TheCapital/bin/Debug/TheCapital.dll ../TheCapital/Mod/Assemblies/
+cp --verbose -rf ../TheCapital/Mod/* "$RW_PATH/Mods/TheCapital"
